87b35cd9d713bdfe289d5090f1fac1a59130f131
Geraldine Macías <geral.oe@gmai.com>
2020-02-16 18:53:57 -0600

    Teams components. PagenotFount component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TeamsComponent } from './components/teams/teams.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'teams', component: TeamsComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'teams' },
+];
+//{ path: '**', component: PageNotFoundComponent },
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { DetailTeamComponent } from './components/teams/detail-team/detail-team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TeamsComponent,
+    PageNotFoundComponent,
+    DetailTeamComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
